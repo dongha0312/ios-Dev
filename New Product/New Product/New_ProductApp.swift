8583a320d56144c213e818a708476915e6e645c5
dongha0312 <rsj1001@gmail.com>
2024-05-08 23:38:01 +0900

    appleproduct

---
+++ New Product/New Product/New_ProductApp.swift
@@ -0,0 +1,17 @@
+//
+//  New_ProductApp.swift
+//  New Product
+//
+//  Created by Dongha Ryu on 5/8/24.
+//
+
+import SwiftUI
+
+@main
+struct New_ProductApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- WeatherForecast/WeatherForecast/ContentView.swift
+++ WeatherForecast/WeatherForecast/ContentView.swift
@@ -22,5 +22,6 @@ struct ContentView: View {
 }
 #Preview {
     ContentView()
+ 
 }
     
